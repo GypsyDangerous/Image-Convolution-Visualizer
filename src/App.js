437ba099d3be6fe29c37639a032e9e756d2593d3
David Snyder <davidgraygs4@gmail.com>
2020-06-13 01:29:53 -0400

    code formatting

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, { useState, useCallback, useRef } from 'react';
-import './App.css';
-import ImageDisplay from './components/ImageDisplay';
-import Convolution from './components/Convolution';
+import React, { useState, useCallback, useRef } from "react";
+import "./App.css";
+import ImageDisplay from "./components/ImageDisplay";
+import Convolution from "./components/Convolution";
 
 function App() {
-  const [dim, setDim] = useState(3)
-  const [filter, setFilter] = useState()
-  const [scale, setScale] = useState()
-  
+  const [dim, setDim] = useState(3);
+  const [filter, setFilter] = useState();
+  const [scale, setScale] = useState();
 
   const inputHandler = useCallback((f, s) => {
-    setFilter(f)
-    setScale(s)
-  }, [])
+    setFilter(f);
+    setScale(s);
+  }, []);
 
-  const changeHandler = e => {
-    const n = Number(e.target.value)
-    if(n %2 === 0){
-      setDim(n+Math.sign(n-dim))
-    }else{
-      setDim(n)
+  const changeHandler = (e) => {
+    const n = Number(e.target.value);
+    if (n % 2 === 0) {
+      setDim(n + Math.sign(n - dim));
+    } else {
+      setDim(n);
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
-      <div className="dimension"><h1 className="title">Image Convolution Demonstration</h1></div>
-     <Convolution dim={dim} filter={filter} scale={scale} onInput={inputHandler}/>
-     <ImageDisplay filter={filter} scale={scale}/>
-      
-      
+      <div className="dimension">
+        <h1 className="title">Image Convolution Demonstration</h1>
+      </div>
+      <Convolution
+        dim={dim}
+        filter={filter}
+        scale={scale}
+        onInput={inputHandler}
+      />
+      <ImageDisplay filter={filter} scale={scale} />
     </div>
   );
 }
